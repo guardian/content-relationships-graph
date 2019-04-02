3984553766f141ae2a89052382bfb26cb04c0b96
Pascal Honoré <pascal@alseyn.net>
2019-04-02 16:24:57 +0100

    Return URLs found in Richlinks

--- src/main/scala/ExtractLinks.scala
+++ src/main/scala/ExtractLinks.scala
@@ -1,9 +1,16 @@
+import com.gu.contentapi.client.model.v1.ElementType.RichLink
 import com.gu.contentapi.client.model.v1.{BlockElement, Content}
 
 object ExtractLinks {
-  def extractLinks(content: Content): Seq[BlockElement] = {
-    content.blocks.map(blocks =>
+  def extractLinks(content: Content): Seq[String] = {
+    val elements = content.blocks.map(blocks =>
       blocks.main.toSeq ++ blocks.body.getOrElse(Seq())
     ).getOrElse(Seq()).flatMap(_.elements)
+    elements.map {element =>
+      element.`type` match {
+        case RichLink => element.richLinkTypeData.get.originalUrl.getOrElse("").toString()
+        case _ => ""
+      }
+    }.filter( _.size != 0  )
   }
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,9 +9,11 @@ object main {
     val client = new GuardianContentClient(key)
     // query for a single content item and print its web title
     val a = ContentApiClient.item("/politics/2019/apr/02/mps-seek-to-stop-no-deal-brexit-by-tabling-article-50-bill").showAtoms("all").showBlocks("all").showElements("all").showPackages(true).showEditorsPicks(true).showReferences("all").showRelated(true).showSection(true).showTags("all")
-      client.getResponse(a).foreach(item =>
-        println(item)
+    client.getResponse(a).foreach(item =>
+      item.content.foreach(content =>
+        ExtractLinks.extractLinks(content).foreach{str => println(str)}
       )
+    )
   }
 }
 
