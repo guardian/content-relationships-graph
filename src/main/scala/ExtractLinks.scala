6c2cc9de07944957dd960467dd371f1508fb5d6f
Pascal Honoré <pascal@alseyn.net>
2019-04-02 14:34:58 +0100

    Few corrections

--- src/main/scala/ExtractLinks.scala
+++ src/main/scala/ExtractLinks.scala
@@ -1,9 +1,9 @@
 import com.gu.contentapi.client.model.v1.Content
 
-    object ExtractLinks {
-      def extractLinks(content: Content): Seq[String] = {
-        val x = content.blocks.map(blocks =>
-          blocks.main.toSeq ++ blocks.body.getOrElse(Seq())
-        ).getOrElse(Seq()).flatMap(_.elements)
-
-      }
+object ExtractLinks {
+  def extractLinks(content: Content): Seq[String] = {
+    val x = content.blocks.map(blocks =>
+      blocks.main.toSeq ++ blocks.body.getOrElse(Seq())
+    ).getOrElse(Seq()).flatMap(_.elements)
+  }
+}
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -8,11 +8,11 @@ object main {
     val key = "asdf"
     val client = new GuardianContentClient(key)
     // query for a single content item and print its web title
-    val a =ContentApiClient.item("/politics/2019/apr/02/mps-seek-to-stop-no-deal-brexit-by-tabling-article-50-bill").showAtoms("all").showBlocks("all").showElements("all").showPackages(true).showEditorsPicks(true).showReferences("all").showRelated(true).showSection(true).showTags("all")
-  client.getResponse(a).foreach(item =>
-
-println(item)
-  )
-
+    val a = ContentApiClient.item("/politics/2019/apr/02/mps-seek-to-stop-no-deal-brexit-by-tabling-article-50-bill").showAtoms("all").showBlocks("all").showElements("all").showPackages(true).showEditorsPicks(true).showReferences("all").showRelated(true).showSection(true).showTags("all")
+      client.getResponse(a).foreach(item =>
+        println(item)
+      )
   }
 }
+
+
