a37882a7bc0fee31d6779835c6aab3e5fb295365
Alex Ware <alex.ware@guardian.co.uk>
2019-04-05 14:46:25 +0100

    restore main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,77 +2,98 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object main {
   def main(args: Array[String]): Unit = {
-    println("hello")
-    val atom = Atom("1b78011e-a4f7-4b7f-8cab-6967274ffd1b", "media")
-    Content.getAtom(atom).map { a =>
-      println(a)
+
+    // Extract CAPI object for one Article and print to the terminal
+    /*
+    Content.getArticle("/politics/2019/apr/03/mps-pass-motion-to-debate-article-50-extension-by-one-vote")
+        .foreach{maybeContent =>
+          maybeContent.foreach{content =>
+            println(content)
+          }
+        }
+     */
+
+    // Extract CAPI objects for a CAPI search term, and print to the screen
+    /*
+    Content.getArticles("brexit")
+      .foreach{seq =>
+        seq.foreach{content =>
+          println(content)
+        }
+      }
+     */
+
+    // Extract Links from Content (CAPI object)
+    /*
+    Content.getArticle("/politics/2019/apr/03/mps-pass-motion-to-debate-article-50-extension-by-one-vote")
+        .foreach{ optContent =>
+          optContent.foreach{ content =>
+            ExtractLinks.extractLinks(content).foreach{ link =>
+              println(link)
+            }
+          }
+        }
+     */
+
+    /*
+    Content.getArticles("brexit").foreach{ articles =>
+      articles.foreach{ content =>
+        Content.getArticle(content.id).map{ oc =>
+          oc.map{ c =>
+            ExtractLinks.extractLinks(c).foreach{ link =>
+              println(link)
+            }
+          }
+        }
+      }
     }
-    //val things = Content.getArticles
-//    things.map { articles =>
-//      articles
-//        .map { article =>
-//          println(article.id)
-//          Content.getArticle(article.id).map { content =>
-//            content.map { c =>
-//              val atoms = Atoms.extractAtoms(c)
-//              println(atoms)
-//              atoms.map { atom =>
-//                println("helo")
-//                val a = Content.getAtom(atom)
-//                println(a)
-//                a.map { aa =>
-//                  println(aa)
-//                }
-//              }
-//            }
-//
-//          }
-//
-//        }
-//    }
+     */
+
+    /*
+    val things = Content.getArticles("brexit")
+    things.map { articles =>
+
+      articles
+        .map { article =>
+          GraphStore
+            .read(s"""
+            |MERGE (a: Page {url:"${article.webUrl}"}) SET a.title="${article.webTitle}", a.path="${article.id}"
+          """.stripMargin)
+        }
+
+      articles.map { article =>
+        val links = Content.getLinksForArticle(article.id)
+        //// Below doesn't work because Content.getLinksForArticle is not currently implemented
+        // links.map { futureLink =>
+        //  futureLink.map { link =>
+        //    GraphStore.read(s"""
+        //                       |MERGE(a: Page {url:"${article.webUrl}"})
+        //                       |MERGE(b: Page {url:"${link.url}"})
+        //                       |MERGE (a)-[:Link {text: "${link.text}", source: "${link.source}"}]->(b)
+        //     """.stripMargin)
+        //  }
+        }
+    }
+     */
+
+    /*
+    GraphStore.read("""
+          |MATCH (n:Page) WHERE NOT EXISTS (n.title) RETURN n.url
+        """.stripMargin).map { result =>
+      result
+        .map(record => record.get("n.url").asString)
+        .filter(_.contains("https://www.theguardian.com"))
+        .map { url =>
+          val path = url.replace("https://www.theguardian.com", "")
+          Content.getArticle(path).map { maybeContent =>
+            maybeContent.map { content =>
+              GraphStore.storeArticle(content)
+              // GraphStore.storeArticleLinks(content)
+            }
+          }
+        }
+    }
+   */
 
   }
-////    val things = Content.getArticles
-////    things.map { articles =>
-////      articles
-////        .map { article =>
-//////          println(path)
-////          GraphStore
-////            .read(s"""
-////            |MERGE (a: Page {url:"${article.webUrl}"}) SET a.title="${article.webTitle}", a.path="${article.id}"
-////          """.stripMargin)
-////
-////        }
-////      articles.map { article =>
-////        val links = Content.getLinksForArticle(article.id)
-////        links.map { futureLink =>
-////          futureLink.map { link =>
-////            GraphStore.read(s"""
-////               |MERGE(a: Page {url:"${article.webUrl}"})
-////               |MERGE(b: Page {url:"${link.url}"})
-////               |MERGE (a)-[:Link {text: "${link.text}", source: "${link.source}"}]->(b)
-////             """.stripMargin)
-////          }
-////        }
-////      }
-//    GraphStore.read("""
-//          |MATCH (n:Page) WHERE NOT EXISTS (n.title) RETURN n.url
-//        """.stripMargin).map { result =>
-//      result
-//        .map(record => record.get("n.url").asString)
-//        .filter(_.contains("https://www.theguardian.com"))
-//        .map { url =>
-//          val path = url.replace("https://www.theguardian.com", "")
-//          Content.getArticle(path).map { maybeContent =>
-//            maybeContent.map { content =>
-//              GraphStore.storeArticle(content)
-////              GraphStore.storeArticleLinks(content)
-//            }
-//          }
-//
-//        }
-//
-//    }
-//  }
-
 }
