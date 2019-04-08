a9e4e73b6b890b7b305157ee1e13656381355cf5
Alex Ware <alex.ware@guardian.co.uk>
2019-04-08 14:47:39 +0100

    make this a program

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,27 +1,41 @@
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Success
 
 object main {
   def main(args: Array[String]): Unit = {
-    Content
-      .getArticles("amazon")
-      .map { seq =>
-        seq.foreach { content =>
-          val path = content.id
-          GraphStore.storePath(path)
-        }
-      }
-
-    GraphStore.read("""
+    Future.sequence(
+      Seq(
+        Content
+          .getArticles("amazon")
+          .map { seq =>
+            seq.foreach { content =>
+              val path = content.id
+              GraphStore.storePath(path)
+            }
+          },
+        GraphStore
+          .read("""
         |MATCH (n:Page) WHERE NOT EXISTS (n.title) RETURN n.url
-      """.stripMargin).map { result =>
-      result
-        .map(record => record.get("n.url").asString)
-        .filter(_.contains("https://www.theguardian.com"))
-        .map { url =>
-          val path = url.replace("https://www.theguardian.com", "")
-          println(path)
-          GraphStore.storePath(path)
-        }
+      """.stripMargin)
+          .map { result =>
+            Future.sequence(
+              result
+                .map(record => record.get("n.url").asString)
+                .filter(_.contains("https://www.theguardian.com"))
+                .map { url =>
+                  val path = url.replace("https://www.theguardian.com", "")
+                  println(path)
+                  GraphStore.storePath(path)
+                })
+
+          }
+          .flatMap(identity)
+      )) andThen {
+      case Success(_) => System.exit(0)
+      case _          => System.exit(1)
     }
+
   }
+
 }
