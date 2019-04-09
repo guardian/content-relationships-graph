f7b1b312f6846042c9cc453abe93f045635f5b71
Pascal Honoré <pascal@alseyn.net>
2019-04-09 11:55:38 +0100

    Pull tags during the CAPI query search

--- src/main/scala/Content.scala
+++ src/main/scala/Content.scala
@@ -31,7 +31,11 @@ object Content {
   }
 
   def getArticles(term: String): Future[Seq[Content]] = {
-    val search = ContentApiClient.search.q(term).pageSize(200)
+    val search = ContentApiClient
+      .search
+      .q(term)
+      .showTags("all")
+      .pageSize(200)
     client.getResponse(search).map { response =>
       response.results
     }
